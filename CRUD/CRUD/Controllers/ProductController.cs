b5c6166b270248313f4fda4a862da8ea3bd25993
RajnikantSerasiya <rserasiya@gmail.com>
2024-06-14 08:10:54 +0530

    mvc lec 17

---
+++ CRUD/CRUD/Controllers/ProductController.cs
@@ -0,0 +1,132 @@
+﻿using CRUD.DAL;
+using CRUD.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CRUD.Controllers
+{
+    public class ProductController : Controller
+    {
+        string connectionString = ConfigurationManager.ConnectionStrings["ShowITConnectionString"].ConnectionString;
+
+        // GET: Product
+        public ActionResult Index()
+        {
+            var list = new List<Product>();
+            var dataSet = new ProductDataSet();
+
+            using (var connection = new SqlConnection(connectionString))
+            {
+                // this is inline query
+                //using (var command = new SqlCommand("SELECT * FROM Products", connection))
+                //{
+                //    command.CommandType = CommandType.Text;
+                //    connection.Open();
+                //    using (var reader = command.ExecuteReader())
+                //    {
+                //        dataSet.Products.Load(reader);
+                //    }
+                //}
+                using (var command = new SqlCommand("GetProducts", connection))
+                {
+                    command.CommandType = CommandType.StoredProcedure;
+                    connection.Open();
+                    using (var reader = command.ExecuteReader())
+                    {
+                        dataSet.Products.Load(reader);
+                    }
+                }
+            }
+            list = ProductRepository.ConvertDataSetToList(dataSet.Products);
+            return View(list);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Product product)
+        {
+            using (var connection = new SqlConnection(connectionString))
+            {
+                using (var command = new SqlCommand("SaveProduct", connection))
+                {
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.Parameters.AddWithValue("@Name", product.Name);
+                    command.Parameters.AddWithValue("@Price", product.Price);
+                    connection.Open();
+                    int rowsaffected = command.ExecuteNonQuery();
+                }
+            }
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Edit(int id)
+        {
+            Product product = new Product();
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                using (SqlCommand command = new SqlCommand("GetProductById", connection))
+                {
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.Parameters.AddWithValue("@Id", id);
+                    connection.Open();
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            product.Id = (int)reader["Id"];
+                            product.Name = reader["Name"].ToString();
+                            product.Price = (decimal)reader["Price"];
+                        }
+                    }
+                }
+            }
+
+            return View(product);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Product product)
+        {
+            using (var connection = new SqlConnection(connectionString))
+            {
+                using (var command = new SqlCommand("SaveProduct", connection))
+                {
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.Parameters.AddWithValue("@Id", product.Id);
+                    command.Parameters.AddWithValue("@Name", product.Name);
+                    command.Parameters.AddWithValue("@Price", product.Price);
+                    connection.Open();
+                    int rowsaffected = command.ExecuteNonQuery();
+                }
+            }
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Delete(int id)
+        {
+            using (var connection = new SqlConnection(connectionString))
+            {
+                using (var command = new SqlCommand("DeleteProduct", connection))
+                {
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.Parameters.AddWithValue("@Id", id);
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                }
+            }
+            return RedirectToAction("Index");
+        }
+
+    }
+}
---
+++ CRUD/CRUD/DAL/ProductDataSet.cs
@@ -0,0 +1,44 @@
+﻿using CRUD.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace CRUD.DAL
+{
+    public class ProductDataSet : DataSet
+    {
+        public DataTable Products { get; set; }
+
+        public ProductDataSet()
+        {
+            Products = new DataTable("Products");
+            // Define columns for the Products table
+            Products.Columns.Add("Id", typeof(int));
+            Products.Columns.Add("Name", typeof(string));
+            Products.Columns.Add("Price", typeof(decimal));
+        }
+    }
+
+    public static class ProductRepository
+    {
+        public static List<Product> ConvertDataSetToList(DataTable dataTable)
+        {
+            List<Product> productList = new List<Product>();
+            
+            foreach (DataRow row in dataTable.Rows)
+            {
+                Product product = new Product
+                {
+                    Id = Convert.ToInt32(row["Id"]),
+                    Name = row["Name"].ToString(),
+                    Price = Convert.ToDecimal(row["Price"])
+                };
+                productList.Add(product);
+            }
+
+            return productList;
+        }
+    }
+}
